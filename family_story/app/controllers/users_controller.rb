80bc98e154710332f5dab91ab8c200fc9f52456b
AdamMarcus <adam.marcus.austin@gmail.com>
2019-04-11 17:21:43 -0600

    Getting username declining existing username to work.

--- family_story/app/controllers/users_controller.rb
+++ family_story/app/controllers/users_controller.rb
@@ -9,17 +9,28 @@ class UsersController < ApplicationController
     # Save new user record
     def create
         # Will not have template, save new account and direct to browse page or back go login page
-        @user = User.new(allowed_params)
+        tempUsername = allowed_params[:username]
+        puts "## #{tempUsername} ##"
+        if User.exists?(username: tempUsername)
+            puts ("## Verified username #{tempUsername} already exists.")
+            flash[:notice] = "Username #{tempUsername} already exists. Please pick a different username."
+            redirect_to users_new_path
+        else
+            puts "## Verified no user by username exists. Creating new user."
+            @user = User.new(allowed_params)
+
+            if @user.save
+                # do something
+                print("Creating a new user " + @user.username + ".")
+                redirect_to stories_path
+            else
+                render 'new'
+            end
+        end
         # Add code here to check if username already exists in DB!
 
         # If the save was successful
-        if @user.save
-            # do something
-            print("Creating a new user " + @user.username + ".")
-            redirect_to stories_path
-        else
-            render 'new'
-        end
+
     end
 
     def loginpage
@@ -27,12 +38,12 @@ class UsersController < ApplicationController
     end
 
     def validatelogin
-        # @user = User.find(params[:username])
-        # if @user
+        @user = User.find_by(username: params[:username])
+        if @user
             #redirect_to stories_path    
-        # else 
-        redirect_to users_testroute_path
-        # end
+        else 
+            redirect_to users_testroute_path
+        end
     end
 
     def testroute
