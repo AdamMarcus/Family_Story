1fc0f40989eb7ed275d1faf73ad4039985e9b162
AdamMarcus <adam.marcus.austin@gmail.com>
2019-04-10 20:25:28 -0600

    Worked on login page and ability to make new account. Got stories/index page working.

--- family_story/app/controllers/users_controller.rb
+++ family_story/app/controllers/users_controller.rb
@@ -9,6 +9,22 @@ class UsersController < ApplicationController
     # Save new user record
     def create
         # Will not have template, save new account and direct to browse page or back go login page
+        @user = User.new(allowed_params)
+        # Add code here to check if username already exists in DB!
+
+        # If the save was successful
+        if @user.save
+            # do something
+            redirect_to stories_path
+        else
+            render 'new'
+        end
     end
+
+    private
+        def allowed_params
+            # Only this one attribute will be allowed, no hacking
+            params.require(:user).permit(:username)
+        end
     
 end
--- family_story/config/routes.rb
+++ family_story/config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   get 'site/writePage'
   get 'site/browse'
 
-  # get 'stories/new'
+  get 'stories/new'
   # get 'stories/index'
+
+  get 'users/new'
 
   root 'site#loginPage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
